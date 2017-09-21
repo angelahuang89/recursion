637a0b3b7e8e0d49b16137ba18ac9116553df84c
Angela Huang <angelahuang89@gmail.com>
2017-09-21 11:25:07 -0700

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+  	return "".concat (obj);
+  } 
+  if (Array.isArray (obj)) {
+  	var massiveString = "[";
+  	for (var i = 0; i < obj.length; ++i) {
+  	  if (i !== 0) {
+  	    massiveString += ","; 
+  	  }
+  		massiveString += stringifyJSON(obj[i]); 
+  	}
+  	massiveString += "]"; 
+  	return massiveString;
+  }
+  if (typeof obj === "object") {
+    var massiveString = "{";
+  	for (var key in obj) {
+  	  if (massiveString !== "{") {
+  	    massiveString += ","; 
+  	  }
+  	  var value = stringifyJSON(obj[key]);
+  	  if (value !== "") massiveString += stringifyJSON(key) + ':' + stringifyJSON(obj[key]); 
+  	}
+  	massiveString += "}"; 
+  	return massiveString;
+  }
+  if (typeof obj === "") {
+  	return "".concat("\"", obj, "\"");
+  }
+  if (obj === undefined || typeof obj === "function") {
+  	return "";
+  }
+  if (typeof obj === "string") {
+    return "\"" + obj + "\""
+  }
+  else {
+  	return "".concat (obj);
+  }
 };
